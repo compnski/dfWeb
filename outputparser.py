91c16dd2b441f5a821255a4b305e240f45bcf508
Jason Freidman <jason@kraken.(none)>
2011-07-18 01:06:33 -0700

    Missed output parser

--- outputparser.py
+++ outputparser.py
@@ -1,58 +1,77 @@
 import re
 from collections import defaultdict
-
+import sys
+import os
 import multiprocessing
-class InputParserServer(object):
+class OutputParserServer(object):
 
     def __init__(self, escape_code_map):
-        self.inputParser = InputParser()
-        self.inputParser.loadEscapeMap(escape_code_map)
+        self.outputParser = OutputParser()
+        self.outputParser.loadEscapeMap(escape_code_map)
         (self.charPipe, self.commandPipe) = multiprocessing.Pipe(True)
+        (self.charPipe_r, self.charPipe_w) = os.pipe()
 
 
     def getPipes(self):
         "Returns a tuple of outPipe, inPipe. Caller writes to outPipe, reads from inPipe"
-        return self.charPipe, self.commandPipe
+        return self.charPipe_w, self.commandPipe
 
     def start(self):
-        self.process = multiprocessing.Process(target=self._run, name="InputParserServer")
+        self.process = multiprocessing.Process(target=self._run, name="OutputParserServer",
+                                               kwargs={"charPipe":self.charPipe_r,
+                                                       "commandPipe":self.commandPipe})
         self.process.start()
+        print >>sys.stderr, "OutputParserServer: %d", self.process.pid
 
     def stop(self):
-        self.charPipe.close()
+        self.charPipe_r.close()
         self.commandPipe.close()
+        self.process.terminate()
         self.process.join()
 
-    def _run(self):
+    def quit(self):
+        f = open("/tmp/undecoded.txt", "w")
+        f.write("TEST")
+        f.write(str(self.outputParser.undecodedMap))
+        f.close()
+
+    def _run(self, charPipe, commandPipe):
         try:
             while True:
-                char = self.charPipe.recv():
-                ret = self.inputParser.parseChar(char)
+                #char = charPipe.read()
+                char = os.read(charPipe, 1)
+                if char == "\d":
+                    return
+                ret = self.outputParser.parseChar(char)
                 if ret is not None:
-                    self.commandPipe.put(ret[0])
-                    self.commandPipe.put(ret[1])
+                    commandPipe.send(ret[0])
+                    commandPipe.send(ret[1])
         except EOFError:
+            print "EOF"
             return
+        finally:
+            commandPipe.send(self.outputParser.undecodedMap)
+            commandPipe.send("QUIT")
 
-class InputParser(object):
 
-    MAX_SEQUENCE = 20
+class OutputParser(object):
+
+    MAX_SEQUENCE = 10
     def __init__(self):
         self.regexList = []
         self.buf = []
-
-    def addEscapeCode(self, key, meaningTuple):
-        regex = self.escapeCodeToRegex(key)
-        regex = r"^%s([^\x1b]*)$" % regex
-        print regex
-        pattern = re.compile(regex)
-        self.regexList.append((pattern, meaningTuple))
 
         self.seqLen = 0
         self.maxSeqLen = 0
         self.maxBuf = ""
 
         self.undecodedMap = defaultdict(int)
+
+    def addEscapeCode(self, key, meaningTuple):
+        regex = self.escapeCodeToRegex(key)
+        regex = r"^%s([^\x1b]*)$" % regex
+        pattern = re.compile(regex)
+        self.regexList.append((pattern, meaningTuple))
 
     def parseChar(self, char):
         if char is None:
@@ -61,32 +80,31 @@ class InputParser(object):
             return self.match("".join(b))
 
         self.seqLen += 1
-        if self.buf and (char == "\x1b" or self.seqLen > InputParser.MAX_SEQUENCE):
+        if self.buf and (char == "\x1b" or self.seqLen > OutputParser.MAX_SEQUENCE):
             b = self.buf
             self.buf = [char]
             if b[0] != "\x1b":
                 self.seqLen = 0
-                return "".join(b)
+                return "".join(b), ""
             match = self.match("".join(b))
-            self.logStats(match)
+            #self.logStats(match)
             return match
         self.buf.append(char)
 
-    def logStats(self, match):
-        self.seqLen -= len(match[1]) #subtract non-sequence chars
-        self.maxSeqLen = max(self.seqLen, self.maxSeqLen)
-        if self.maxSeqLen == self.seqLen:
-            self.maxBuf = b
-        self.seqLen = 0
-        if type(match) == str and match[0] == "\x1b":
-            self.logUndecoded(str)
-        return match
+#     def logStats(self, match):
+#         self.seqLen -= len(match[1]) #subtract non-sequence chars
+#         self.maxSeqLen = max(self.seqLen, self.maxSeqLen)
+#         if self.maxSeqLen == self.seqLen:
+#             self.maxBuf = b
+#         self.seqLen = 0
+#         if type(match) == str and match[0] == "\x1b":
+#             self.logUndecoded(str)
+#         return match
 
     def match(self, buf):
         for (pattern, actionTuple) in self.regexList:
             matcher = pattern.match(buf)
             if matcher is not None:
-                #print matcher.groups()
                 return (actionTuple + tuple(matcher.groups()[:-1]), matcher.groups()[-1])
         return buf, ""
 
